8363fa0b53483f80c7df7239814c74c5ff7f5d8c
MorningFrog <1098224028@qq.com>
2025-08-10 11:16:27 +0800

    test linux arm64

--- cibuildwheel_scripts/before-all.linux.bash
+++ cibuildwheel_scripts/before-all.linux.bash
@@ -29,7 +29,5 @@ cd "$VCPKG_ROOT"
 env -u CMAKE_TOOLCHAIN_FILE -u CMAKE_PREFIX_PATH -u CMAKE_MODULE_PATH \
   ./bootstrap-vcpkg.sh -disableMetrics
 
-echo "set(VCPKG_BUILD_TYPE release)" | tee -a triplets/${VCPKG_TARGET_TRIPLET:-x64-linux}.cmake
-
 # install eigen3
 "$VCPKG_ROOT/vcpkg" install "eigen3:${VCPKG_TARGET_TRIPLET}"
